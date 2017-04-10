1cc97ed687b587041005132e91f1e2c1d540e119
Alex Bush <alexandrabush1@gmail.com>
2017-04-09 23:10:07 -0400

    added predictive search + bonus clickable google links

--- assignment4.js
+++ assignment4.js
@@ -1,17 +1,37 @@
-// The anonymous function below will fire on page load
-
-// Some things to consider
-// $.ajax(); to make your requests a little easier. Or the vanilla js way, it's up to you.
-// $.on(); for event handling
-// Remember, selecting elements in jQuery is like selecting them in CSS
-// You'll probably have to manipulate some strings
-// some jQuery functions to help display results
-// $.show(), $.hide(), $.slideup(), $.slidedown(), $.fadein(), $.fadeout()
-// Add content from requests with something like
-// $.html(), $.text(), etc.
-// keyup events could be helpful to get value of field as the user types
+// gets predictive results that link to google
 
 (function() {
-  // Magic!
-  console.log('Keepin\'n it clean with an external script!');
+ 	
+	//When the user types something, grab what's been typed and get the json
+ 	$('.flexsearch-input').keyup(function(e) {
+    	var searchVal = $('.flexsearch-input').val();
+    	searchVal = searchVal.toLowerCase();
+    	$('.predictive-results').html("");
+    	grabResults(searchVal);
+    });
+
+ 	// loop through JSON data and add any matching things to the div
+    function grabResults(searchVal) {
+
+    	// empty google link that we can tack a search result on to
+    	googleMeLink = "https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q="
+
+    	// grab the json data using a get
+    	$.getJSON("http://www.mattbowytz.com/simple_api.json?data=all", function( data ) {
+		 	//loop through the json
+		 	for (var i in data) {
+  				for (var j in data[i]) {
+  					var possibleResults = data[i][j];
+  					for( var k in possibleResults){
+  						// if the current piece of data matches the current search, add it to the div
+  						var currItem = possibleResults[k];
+  						currItem = currItem.toLowerCase();
+  						if(currItem.startsWith(searchVal) && searchVal.length > 0 && currItem.length > 1) {
+                			$('.predictive-results').append('<a target="_blank" href=' + googleMeLink + currItem.replace(' ', '%20') + '>' + currItem + '</a><br>');
+		 				}
+  					}
+   				}
+			}		
+		});
+	}
 })();
